fd84fb4eaa57812f53d3cd7cab57134532082b2b
S10143806H <644165318.tj@gmail.com>
2021-12-04 17:06:19 +0800

    edit latex make in conf.py

--- doc/zh_TW/source/conf.py
+++ doc/zh_TW/source/conf.py
@@ -68,12 +68,17 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#
 html_static_path = []
+
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
 
 # -- Options for Latex output -------------------------------------------------
 #
-latex_engine = "pdflatex"
+# latex_engine = "pdflatex"
 
 # -- Options for EPUB output -------------------------------------------------
 #
